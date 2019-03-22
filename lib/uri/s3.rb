40ebb1fdf873e92657bb45fe03c35c9c4ae975db
Scott Brickner <scottb@brickner.net>
2019-03-22 17:02:15 -0400

    Add download method

--- lib/uri/s3.rb
+++ lib/uri/s3.rb
@@ -9,10 +9,21 @@ module URI
     end
 
     def s3_object(options = {})
-      @s3_object ||= s3_bucket(options).object(URI.decode_www_form_component(path[1..-1]))
+      @s3_object ||= s3_bucket(options).object(s3_key)
+    end
+
+    def download_file(filename, options = {})
+      client_options = options.fetch(:client, {})
+      get_params = options.fetch(:params, {})
+      file_options = options.fetch(:file, {})
+      Aws::S3::Client.new(client_options).get_object(get_params).download_file(filename, file_options)
     end
 
     private
+
+    def s3_key
+      URI.decode_www_form_component(path[1..-1])
+    end
 
     def s3_resource(options = {})
       @s3_resource ||= Aws::S3::Resource.new(options)
